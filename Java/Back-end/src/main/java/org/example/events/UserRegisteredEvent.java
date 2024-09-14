86985fd5bc48234d6e34aeb5ca5fe7026a424085
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2024-09-14 16:14:43 -0300

    "Adição do método de envio de e-mails..."

---
+++ Java/Back-end/src/main/java/org/example/events/UserRegisteredEvent.java
@@ -0,0 +1,37 @@
+package org.example.events;
+
+import org.example.model.User;
+import org.springframework.context.ApplicationEvent;
+
+
+/**
+ * Evento que representa o registro de um novo usuário.
+ * <p>
+ * Este evento é acionado quando um novo usuário é registrado no sistema e
+ * pode ser usado para desencadear ações pós-registro, como enviar um e-mail
+ * de boas-vindas ou executar outras tarefas relacionadas ao usuário recém-criado.
+ * </p>
+ */
+public class UserRegisteredEvent extends ApplicationEvent {
+
+    private final User user;
+
+    /**
+     * Construtor para criar um novo evento de registro de usuário.
+     *
+     * @param user o objeto {@link User} associado a este evento.
+     */
+    public UserRegisteredEvent ( User user ) {
+        super( user );
+        this.user = user;
+    }
+
+    /**
+     * Retorna o usuário associado a este evento.
+     *
+     * @return o objeto {@link User} que foi registrado.
+     */
+    public User getUser () {
+        return user;
+    }
+}
---
+++ Java/Back-end/src/main/java/org/example/listeners/UserRegistrationListener.java
@@ -0,0 +1,43 @@
+package org.example.listeners;
+
+import org.example.events.UserRegisteredEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+/**
+ * Ouvinte de eventos de registro de usuário.
+ * <p>
+ * Esta classe escuta eventos do tipo {@link UserRegisteredEvent} e envia um e-mail quando um novo usuário
+ * é registrado na aplicação.
+ * </p>
+ */
+@Component
+public class UserRegistrationListener {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    /**
+     * Manipula eventos do tipo {@link UserRegisteredEvent}.
+     * <p>
+     * Quando um evento {@code UserRegisteredEvent} é recebido, este método cria uma mensagem de e-mail
+     * informando sobre o novo usuário registrado e envia o e-mail para o endereço especificado.
+     * </p>
+     *
+     * @param event o evento {@code UserRegisteredEvent} que contém informações sobre o novo usuário registrado
+     */
+    @EventListener
+    public void handleUserRegisteredEvent ( UserRegisteredEvent event ) {
+        String userEmail = event.getUser().getUsername();
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo( "leonardo.bernardo2658@gmail.com" );
+        message.setSubject( "Novo usuário cadastrado" );
+        message.setText( "Um novo usuário foi cadastrado com o username: " + userEmail );
+
+        mailSender.send( message );
+    }
+}
